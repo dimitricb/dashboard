812a003765ae6fd00e17f2f8bf16cc7eee8ab214
Bojana Dimitric <bojanadimitric00@gmail.com>
2024-12-10 15:46:59 +0100

    Overview page, Project page

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { Route, Routes } from "react-router-dom";
+import OverviewPage from "./pages/OverviewPage";
+import ProjectsPage from "./pages/ProjectsPage";
+import Sidebar from "./components/common/Sidebar";
+
+function App(name) {
+  return (
+    <div className="flex h-screen bg-gray-900 text-gray-100 overflow-hidden">
+      {/* Backgound */}
+      <div className="fixed inset-0 z-0">
+        <div className="absolute inset-0 bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 opacity-80" />
+        <div className="absolute inset-0 backdrop-blur-sm" />
+      </div>
+
+      <Sidebar />
+
+      <Routes>
+        <Route path="/" element={<OverviewPage />} />
+        <Route path="/products" element={<ProjectsPage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
